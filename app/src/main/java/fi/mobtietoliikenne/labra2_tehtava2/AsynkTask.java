73a20e639a62191f9012127973397a903b35d4cc
Niko Kontu <t7koni00@students.oamk.fi>
2019-12-09 20:10:31 +0200

    Done! Fix the bugs. Parsing works fine now!

---
+++ app/src/main/java/fi/mobtietoliikenne/labra2_tehtava2/AsynkTask.java
@@ -0,0 +1,87 @@
+package fi.mobtietoliikenne.labra2_tehtava2;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class AsynkTask extends AsyncTask<String, String, ArrayList<String>> {
+
+    @Override
+    protected ArrayList<String> doInBackground(String... strings) {
+            ArrayList<String> result = null;
+
+        try {
+            URL url = new URL("https://financialmodelingprep.com/api/company/price/AAPL,GOOG,FB,NOK?datatype=json");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream stream = urlConnection.getInputStream();
+            result = jsonParse(fromStream(stream));
+            Log.d("DEBUG",""+result.get(0));
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public ArrayList<String> jsonParse(String dataString) {
+        try {
+
+            JSONObject jsonObject = new JSONObject(dataString);
+
+            Iterator iterator = jsonObject.keys();
+            ArrayList<String> arrayList = new ArrayList<>();
+
+            while (iterator.hasNext()) {
+                String key = (String) iterator.next();
+                JSONObject stockKeys = jsonObject.getJSONObject(key);
+                Log.d("JSONPARSE", "JsonKeys: " + key + " " + stockKeys.getDouble("price"));
+                arrayList.add(key + " " +stockKeys.getDouble("price"));
+            }
+            return arrayList;
+        }
+
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    //From: https://stackoverflow.com/questions/309424/how-do-i-read-convert-an-inputstream-into-a-string-in-java
+    public static String fromStream(InputStream in) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        StringBuilder out = new StringBuilder();
+        String newLine = System.getProperty("line.separator");
+        String line;
+        while ((line = reader.readLine()) != null) {
+            out.append(line);
+            out.append(newLine);
+        }
+        return out.toString();
+    }
+
+    @Override
+    protected void onPostExecute(ArrayList<String> keys) {
+        super.onPostExecute(keys);
+        String lista = "";
+        for (String stock:keys){
+            lista = lista + stock +"\n";
+        }
+        MainActivity.tvOutput2.setText(lista);
+    }
+}
--- app/src/main/java/fi/mobtietoliikenne/labra2_tehtava2/MainActivity.java
+++ app/src/main/java/fi/mobtietoliikenne/labra2_tehtava2/MainActivity.java
@@ -3,12 +3,28 @@ package fi.mobtietoliikenne.labra2_tehtava2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static TextView tvOutput2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnStartAction).setOnClickListener(this);
+        tvOutput2 = (TextView) findViewById(R.id.tvOutput2);
+    }
+
+    @Override
+    public void onClick(View v) {
+        AsynkTask task = new AsynkTask();
+        task.execute();
     }
 }
